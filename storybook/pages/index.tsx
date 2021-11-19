3b86fd506cd0b2d6c0429bda34c5e1039c12b38b
Spiritusanti <mccrackenjacob@gmail.com>
2021-11-18 22:11:45 -0800

    Revert "preparing to revert" correcting compatibility issue with roughjs and nextjs This reverts commit 2ae1b31c778d8c69413f23f4d2e44682be5c855e.

--- storybook/pages/index.tsx
+++ storybook/pages/index.tsx
@@ -1,20 +1,11 @@
 import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
 import PaintCanvas from '../components/PaintCanvas.component'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const [showPaintCanvas, setShowPaintCanvas] = useState(false);
-
-  useEffect(() => {
-    setShowPaintCanvas(true);
-  }, [])
-
-
   return (
     <article>
-      {!showPaintCanvas && <p>Loading...</p>}
-      {showPaintCanvas && <PaintCanvas />}
+      <PaintCanvas />
     </article>
   )
 }
