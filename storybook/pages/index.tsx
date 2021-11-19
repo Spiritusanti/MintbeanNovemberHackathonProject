10c98f01cbc4368e1ad6f3cf195d97626227f9a0
Spiritusanti <mccrackenjacob@gmail.com>
2021-11-18 22:13:03 -0800

    Revert "Revert "preparing to revert"" returning to app state prior to roughjs inclusion This reverts commit 3b86fd506cd0b2d6c0429bda34c5e1039c12b38b.

--- storybook/pages/index.tsx
+++ storybook/pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 import PaintCanvas from '../components/PaintCanvas.component'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [showPaintCanvas, setShowPaintCanvas] = useState(false);
+
+  useEffect(() => {
+    setShowPaintCanvas(true);
+  }, [])
+
+
   return (
     <article>
-      <PaintCanvas />
+      {!showPaintCanvas && <p>Loading...</p>}
+      {showPaintCanvas && <PaintCanvas />}
     </article>
   )
 }
