10882a2a9eee6e2b396fb1ae64288897c382c33d
Chris Toribio <44413949+cromtoribio@users.noreply.github.com>
2021-11-22 10:44:37 -0800

    gen commit

--- storybook/pages/StorybookApp.tsx
+++ storybook/pages/StorybookApp.tsx
@@ -1,50 +1,61 @@
-import { NextPage } from 'next';
-import { useState, useEffect } from 'react';
-import PaintCanvas from '../components/PaintCanvas.component';
-import Header from '../components/Header.component';
-import Prompt from '../components/Prompt.component';
+import { NextPage } from "next";
+import { useState, useEffect } from "react";
+import PaintCanvas from "../components/PaintCanvas.component";
+import Header from "../components/Header.component";
+import Prompt from "../components/Prompt.component";
 
 const StorybookApp: NextPage = () => {
-    const [showPaintCanvas, setShowPaintCanvas] = useState(false);
-    const [currentPromptNumber, setCurrentPromptNumber] = useState<number>(1);
-    const [canvasIsSaved, setCanvasIsSaved] = useState(false);
-    const story = { title: "The tortoise and the Hare", prompts: ["lorem ipsum", "dolor set", "imperetus destinatus", "Et tu brutus"] }
-    const promptTracker = `
+	const [showPaintCanvas, setShowPaintCanvas] = useState(false);
+	const [currentPromptNumber, setCurrentPromptNumber] = useState<number>(1);
+	const [canvasIsSaved, setCanvasIsSaved] = useState(false);
+	const story = {
+		title: "The Tortoise and the Hare",
+		prompts: [
+			"lorem ipsum",
+			"dolor set",
+			"imperetus destinatus",
+			"Et tu brutus",
+		],
+	};
+	const promptTracker = `
         ${currentPromptNumber} of ${story.prompts.length}
-    `
-    useEffect(() => {
-        setShowPaintCanvas(true);
-    }, [])
+    `;
+	useEffect(() => {
+		setShowPaintCanvas(true);
+	}, []);
 
-    // saveCanvas handler - will need to add in canvas saving capabilities;
-    const onSaveCanvas = () => {
-        setCanvasIsSaved(true);
-    }
+	// saveCanvas handler - will need to add in canvas saving capabilities;
+	const onSaveCanvas = () => {
+		setCanvasIsSaved(true);
+	};
 
-    // nextScene Handler
-    const onNextScene = () => {
-        if (currentPromptNumber < story.prompts.length) {
-            setCurrentPromptNumber((prevState) => prevState + 1);
-            setCanvasIsSaved(false);
-        }
-    }
+	// nextScene Handler
+	const onNextScene = () => {
+		if (currentPromptNumber < story.prompts.length) {
+			setCurrentPromptNumber((prevState) => prevState + 1);
+			setCanvasIsSaved(false);
+		}
+	};
 
-    return (
-        <article>
-            <nav className="nav">
-                <Header title={story.title} />
-            </nav>
-            <section>
-                <Prompt prompt={story.prompts[currentPromptNumber]} promptTracker={promptTracker} />
-                {!showPaintCanvas && <p>Loading...</p>}
-                {showPaintCanvas && <PaintCanvas />}
-                <div className="flex-col-center buttonContainer">
-                    {!canvasIsSaved && <button onClick={onSaveCanvas}>Save</button>}
-                    {canvasIsSaved && <button onClick={onNextScene}>Next Scene</button>}
-                </div>
-            </section>
-        </article>
-    )
-}
+	return (
+		<article>
+			<nav className="nav">
+				<Header title={story.title} />
+			</nav>
+			<section>
+				<Prompt
+					prompt={story.prompts[currentPromptNumber]}
+					promptTracker={promptTracker}
+				/>
+				{!showPaintCanvas && <p>Loading...</p>}
+				{showPaintCanvas && <PaintCanvas />}
+				<div className="flex-col-center buttonContainer">
+					{!canvasIsSaved && <button onClick={onSaveCanvas}>Save</button>}
+					{canvasIsSaved && <button onClick={onNextScene}>Next Scene</button>}
+				</div>
+			</section>
+		</article>
+	);
+};
 
 export default StorybookApp;
