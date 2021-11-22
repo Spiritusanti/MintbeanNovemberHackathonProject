9e11093378c0d94ef2d128872f431ce1fd9bce15
Chris Toribio <44413949+cromtoribio@users.noreply.github.com>
2021-11-22 11:11:58 -0800

    fixed styling issues on landing page and app

--- storybook/pages/StorybookApp.tsx
+++ storybook/pages/StorybookApp.tsx
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import { NextPage } from "next";
-import { useState, useEffect } from "react";
-import PaintCanvas from "../components/PaintCanvas.component";
-import Header from "../components/Header.component";
-import Prompt from "../components/Prompt.component";
-
-const StorybookApp: NextPage = () => {
-	const [showPaintCanvas, setShowPaintCanvas] = useState(false);
-	const [currentPromptNumber, setCurrentPromptNumber] = useState<number>(1);
-	const [canvasIsSaved, setCanvasIsSaved] = useState(false);
-	const story = {
-		title: "The Tortoise and the Hare",
-		prompts: [
-			"lorem ipsum",
-			"dolor set",
-			"imperetus destinatus",
-			"Et tu brutus",
-		],
-	};
-	const promptTracker = `
-        ${currentPromptNumber} of ${story.prompts.length}
-    `;
-	useEffect(() => {
-		setShowPaintCanvas(true);
-	}, []);
-
-	// saveCanvas handler - will need to add in canvas saving capabilities;
-	const onSaveCanvas = () => {
-		setCanvasIsSaved(true);
-	};
-
-	// nextScene Handler
-	const onNextScene = () => {
-		if (currentPromptNumber < story.prompts.length) {
-			setCurrentPromptNumber((prevState) => prevState + 1);
-			setCanvasIsSaved(false);
-		}
-	};
-
-	return (
-		<article>
-			<nav className="nav">
-				<Header title={story.title} />
-			</nav>
-			<section>
-				<Prompt
-					prompt={story.prompts[currentPromptNumber]}
-					promptTracker={promptTracker}
-				/>
-				{!showPaintCanvas && <p>Loading...</p>}
-				{showPaintCanvas && <PaintCanvas />}
-				<div className="flex-col-center buttonContainer">
-					{!canvasIsSaved && <button onClick={onSaveCanvas}>Save</button>}
-					{canvasIsSaved && <button onClick={onNextScene}>Next Scene</button>}
-				</div>
-			</section>
-		</article>
-	);
-};
-=======
 import { NextPage } from 'next';
 import { useState, useEffect, Fragment } from 'react';
 import PaintCanvas from '../components/PaintCanvas.component';
@@ -177,6 +116,5 @@ const StorybookApp: NextPage = () => {
         </article>
     )
 }
->>>>>>> 5776b5301065466fc45cee1acb5d11b30baed033
 
 export default StorybookApp;
